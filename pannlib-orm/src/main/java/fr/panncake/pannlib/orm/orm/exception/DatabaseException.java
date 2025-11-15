ba2d1080d1f50366c3545b1b5cbf1955e707868c
itspanncake <72684309+itspanncake@users.noreply.github.com>
2025-11-15 19:29:12 +0100

    Create database exception class

---
+++ pannlib-orm/src/main/java/fr/panncake/pannlib/orm/orm/exception/DatabaseException.java
@@ -0,0 +1,11 @@
+package fr.panncake.pannlib.orm.orm.exception;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
