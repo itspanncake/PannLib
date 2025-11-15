689cebedb5070196b9531c8f6143ab7d2a4b8acc
itspanncake <72684309+itspanncake@users.noreply.github.com>
2025-11-15 22:32:25 +0100

    Create @ForeignKey and @Index annotations

---
+++ pannlib-orm/src/main/java/fr/panncake/pannlib/orm/annotations/ForeignKey.java
@@ -0,0 +1,15 @@
+package fr.panncake.pannlib.orm.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ForeignKey {
+    Class<?> referenced();
+    String referencedColumn() default "id";
+    String name() default "";
+    boolean onDeleteCascade() default false;
+}
---
+++ pannlib-orm/src/main/java/fr/panncake/pannlib/orm/annotations/Index.java
@@ -0,0 +1,13 @@
+package fr.panncake.pannlib.orm.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Index {
+    String name() default "";
+    boolean unique() default false;
+}
